e1aba44d2130ee7b4b82e3bc13e658edb084c83d
LinggaAS <andikalingga021@gmail.com>
2020-01-30 14:16:52 +0700

    membuat database

--- Ngobar/Controllers/AccountController.cs
+++ Ngobar/Controllers/AccountController.cs
@@ -1,16 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Ngobar.Models;
+using Ngobar.Data.Models;
 using Ngobar.Models.AccountViewModels;
 using Ngobar.Services;
 
