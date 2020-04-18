0536262796b5ce8819b8aa93f64a2483c7c06c6b
LinggaAS <andikalingga021@gmail.com>
2020-04-18 15:54:09 +0700

    Register user, Upload services

--- Ngobar.Service/UploadService.cs
+++ Ngobar.Service/UploadService.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Ngobar.Data;
 
 namespace Ngobar.Service
 {
-    class UploadService
+    public class UploadService : IUpload
     {
     }
 }
